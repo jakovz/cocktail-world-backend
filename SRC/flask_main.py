ef6001d8306dcaaaf79d05cb011458cff1eb75fa
yuvalzifruttau <33434382+yuvalzifruttau@users.noreply.github.com>
2018-12-29 16:48:19 +0200

    Update flask_main.py

--- SRC/flask_main.py
+++ SRC/flask_main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 import json
+import DBConnection
 import queries
-import DBconnection
 
 app = Flask(__name__)
 
@@ -10,36 +10,22 @@ app = Flask(__name__)
 def main_page():
     return 'Hello World!'
 
-
+# return: A json representing all the ingredients exist in the DB.
 @app.route('/ingredients')
 def get_ingredients():
-    query = queries.temp1
 
-    x = DBConnection.execute_query(query, 7)
-    # t = DBConnection.rowsToJson(x)
-    """
-    :return: A json representing all the ingredients exist in the DB.
-    """
-    # here should come a db query
-    # return json.dumps("THE_QUERY_RESULT1")
-    return x
+    return DBConnection.execute_query(queries.allIngredients)
 
 
 @app.route('/filters')
 def get_available_filters():
+
     """
     :return: A json representing all the filters we support
     """
     # here should come a db query
 
 
-    #                            Converting MySQL query to JSON:
-    # row_headers = [x[0] for x in cur.description]  # this will extract row headers
-    # rv = cur.fetchall()
-    # json_data = []
-    # for result in rv:
-    #     json_data.append(dict(zip(row_headers, result)))
-    #
     return json.dumps("THE_FILTERS")
 
 
