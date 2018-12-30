14504a99216ea31a3f9fa2d053140022b515f153
jakovz <jakov17@gmail.com>
2018-12-30 19:44:26 +0200

    * added filtering by ingredients + flask endpoint * added get all ingredients query + flask endpoint

--- SRC/flask_main.py
+++ SRC/flask_main.py
@@ -5,46 +5,70 @@ import queries
 
 app = Flask(__name__)
 
+AVAILABLE_FILTERS = ["Calories"]
+
 
 @app.route('/')
 def main_page():
     return 'Hello World!'
 
+
 # return: A json representing all the ingredients exist in the DB.
 @app.route('/ingredients')
 def get_ingredients():
-
-    return DBConnection.execute_query(queries.allIngredients)
+    return DBConnection.execute_query(queries.all_ingredients)
 
 
 @app.route('/filters')
 def get_available_filters():
-
     """
     :return: A json representing all the filters we support
     """
     # here should come a db query
+    return json.dumps(AVAILABLE_FILTERS)
 
 
-    return json.dumps("THE_FILTERS")
+def get_cocktails_by_ingredients(ingredients):
+    return DBConnection.execute_query(queries.get_cocktails_by_ingredients(ingredients), *ingredients)
+
+
+def get_cocktails_by_filters(filters):
+    pass
+
+
+def get_cocktails_by_filters_and_ingredients(filters, ingredients):
+    pass
 
 
 @app.route('/cocktails')
 def get_cocktails():
     """
-    The url is of the form: "/cocktails?ingredients=LIST_OF_INGREDIENTS&filters=LIST_OF_FILTERS"
+    The url is of the form:
+    /cocktails?ingredient={"ingredients":[LIST_OF_INGREDIENTS]}&filter={"filters": [LIST_OF_FILTERS]}
     :return: json representing all matching cocktails
     """
-    try:
-        ingredients = request.args.get('ingredients')
-        filters = request.args.get('filters')
-    except Exception as e:
-        return "Error: wrong arguments were given."
-
-    # here should come a db query
-    return json.dumps("THE_COCKTAILS")
+    ingredients = None
+    filters = None
+    if len(request.args) == 0:
+        return DBConnection.execute_query(queries.all_cocktails)
+    if "ingredient" in request.args:
+        ingredients = json.loads(request.args.get('ingredient'))['ingredients']
+        print(ingredients)
+    if "filter" in request.args:
+        filters = request.args.get('filters')['filters']
+        print(filters)
+    if ingredients and not filters:
+        cocktails = get_cocktails_by_ingredients(ingredients)
+    elif filters and not ingredients:
+        cocktails = get_cocktails_by_filters(filters)
+    else:
+        cocktails = get_cocktails_by_filters_and_ingredients(filters, ingredients)
+    if cocktails is not None:
+        return cocktails
+    else:
+        return "An error occurred during querying the DB."
 
 
 if __name__ == '__main__':
     app.run(debug=True, port="8000")
-    app.run(threaded=True) # GOOD YUVAL
+    app.run(threaded=True)  # GOOD YUVAL
--- SRC/queries.py
+++ SRC/queries.py
@@ -1,1 +1,12 @@
-allIngredients = "SELECT * FROM ingredients"
+all_ingredients = "SELECT * FROM ingredients"
+all_cocktails = "SELECT * FROM drinks"
+
+
+def get_cocktails_by_ingredients(ingredients):
+    cocktails_by_ingredients = "SELECT DISTINCT drinks.name, drinks.drink_img_url " \
+                               "FROM drinks, cocktails_ingredients,ingredients " \
+                               "WHERE drinks.id=cocktails_ingredients.cocktail_id " \
+                               "AND cocktails_ingredients.ingredient_id=ingredients.id "
+    for ingredient in ingredients:
+        cocktails_by_ingredients += "AND ingredients.name='%s' "
+    return cocktails_by_ingredients
