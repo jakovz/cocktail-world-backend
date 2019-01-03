b365e26a36f7384e7bc66bbb9d96ac99bb169ae6
idoos <idoosams@gmail.com>
2019-01-03 15:20:59 +0200

    added some querys

--- SRC/queries.py
+++ SRC/queries.py
@@ -42,3 +42,53 @@ def get_cocktails_by_filters(filters):
                             "GROUP BY drinks.id" \
                             ")".format(filter_name))
     return cocktails_by_filters
+
+
+def get_ingredients_by_cocktails_commonness(cocktails_commonness, ingredient_commonness):
+    return f"""SELECT DISTINCT ingredients.name, ingredients.ingredient_img_url
+                FROM ingredients, (SELECT DISTINCT cocktails_ingredients.ingredient_name AS name
+                FROM cocktails_ingredients
+                WHERE cocktails_ingredients.cocktail_id IN (SELECT cocktail_view.id
+                FROM (SELECT cocktails_ingredients.cocktail_id AS id, COUNT(*) AS count
+                FROM cocktails_ingredients
+                GROUP BY id
+                HAVING count>={cocktails_commonness}) AS cocktail_view) AND
+                cocktails_ingredients.ingredient_name IN (SELECT DISTINCT ingredient_view.name AS name
+                FROM (SELECT cocktails_ingredients.ingredient_name AS name, COUNT(*) AS count
+                FROM cocktails_ingredients
+                GROUP BY name
+                HAVING count>={ingredient_commonness}) AS ingredient_view)) AS T
+                WHERE T.name=ingredients.name"""
+
+
+def get_amount_per_categories(categories, alcoholic):
+    categories_str = ""
+    for i, category in enumerate(categories):
+        if i != 0:
+            categories_str += ", "
+        categories_str += "\""+categories[i]+"\""
+
+    alcoholic_str = "Alcoholic" if alcoholic else "Non alcoholic"
+    return f"""SELECT drinks.category, count(*) as amount
+                FROM drinks
+                WHERE drinks.is_alcoholic = "{alcoholic_str}" AND
+                        drinks.category IN ({categories_str})
+                GROUP BY drinks.category
+                ORDER BY amount 
+                DESC"""
+
+
+def get_drinks_categories():
+    return "SELECT DISTINCT drinks.category FROM drinks"
+
+def get_drinks_categories():
+    return "SELECT DISTINCT meals.category FROM meals"s
+
+def get_glasses_types():
+    return "SELECT DISTINCT drinks.glass_type FROM drinks"
+
+
+
+
+
+
