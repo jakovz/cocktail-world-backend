0cca6696aff1bfe109fde2f7a5e946ab0f2de001
jakovz <jakov17@gmail.com>
2019-01-05 22:45:58 +0200

    * added some examples for url parameters fetching

--- SRC/flask_main.py
+++ SRC/flask_main.py
@@ -7,8 +7,6 @@ import queries
 app = Flask(__name__)
 CORS(app)
 
-AVAILABLE_FILTERS = ["Calories"]
-
 
 @app.route('/')
 def main_page():
@@ -24,14 +22,28 @@ def get_ingredients():
     return json.dumps(ingredients_dict)
 
 
+@app.route('/cocktail_categories')
+def get_cocktail_categories():
+    cocktail_categories = DBConnection.execute_query(queries.get_drinks_categories())
+    print(cocktail_categories)
+    cocktail_categories = [category['category'] for category in json.loads(cocktail_categories)]
+    categories_dict = {"allowed_cocktail_categories": cocktail_categories}
+    return json.dumps(categories_dict)
+
 
 @app.route('/calories_alcoholic')
+def get_calories_alcoholic():
+    pass
 
 
 @app.route('/cocktail_amount_by_glass_categories')
 def get_cocktail_amount_by_glass_categories():
-    glass_categories = DBConnection.execute_query(queries.query_cocktail_amount_by_glass_categories(["Ordinary Drink", "Shot"], True))
-    return json.dumps()
+    if 'categories' not in request.args:
+        return
+    categories = request.args.get('categories')
+    glass_categories = DBConnection.execute_query(
+        queries.query_cocktail_amount_by_glass_categories(categories, True))
+    return json.dumps(glass_categories)
 
 
 @app.route('/ingredients_difference')
@@ -42,24 +54,38 @@ def get_ingredients_difference():
 
 @app.route('/most_used_non_alcoholic')
 def get_most_used_non_alcoholic():
-    glass_categories = DBConnection.execute_query(queries.query_most_used_non_alcoholic())
-    return json.dumps()
+    most_used_glass = DBConnection.execute_query(queries.query_most_used_non_alcoholic())
+    return json.dumps(most_used_glass)
 
 
 @app.route('/categories_by_average_number_of_ingredients')
 def get_categories_by_average_number_of_ingredients():
-    glass_categories = DBConnection.execute_query(queries.categories_by_average_number_of_ingredients(["Ordinary Drink", "Shot"]))
-    return json.dumps()
+    if 'categories' not in request.args:
+        return
+    categories = request.args.get('categories')
+    categories_by_avg_ingredients = DBConnection.execute_query(
+        queries.query_categories_by_average_number_of_ingredients(categories))
+    return json.dumps(categories_by_avg_ingredients)
+
 
 @app.route('/easy_to_make_from_category')
 def get_easy_to_make_from_category():
+    if ('cocktail_categoris' not in request.args) or ('meal_categories' not in request.args):
+        return
+    cocktail_categories = request.args.get('cocktail_categories')
+    meal_categories = request.args.get('meal_categories')
     glass_categories = DBConnection.execute_query(queries.query_easy_to_make_from_category("Beef", "Shot"))
-    return json.dumps()
+    return json.dumps('')
+
 
 @app.route('/full_text_search')
+def full_text_search():
+    pass
 
 
 @app.route('/common_ingredients')
+def common_ingredients():
+    pass
 
 
 if __name__ == '__main__':
