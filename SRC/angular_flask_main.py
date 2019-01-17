c57bf3ab30126097f9df1b46d9b20c14ebee3eda
jakovz <jakov17@gmail.com>
2019-01-17 13:47:52 +0200

    * added the static html and javascript files to the project  * added angular flask app for serving the files  * made all urls be handled by the angular app

--- SRC/angular_flask_main.py
+++ SRC/angular_flask_main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_cors import CORS
+
 app = Flask(__name__, template_folder="../Templates", static_folder="../static")
 CORS(app)
 
@@ -7,6 +8,11 @@ CORS(app)
 @app.route('/')
 def main_page():
     return render_template("index.html")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return main_page()
 
 
 if __name__ == '__main__':
