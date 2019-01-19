d98eb64991f6714edb8b6060a8c2626268598182
yuvalHelman <yuvalasaf3@gmail.com>
2019-01-19 10:40:07 +0200

    Commit before adding index on tables. This mainly works now.

--- SRC/DBConnection.py
+++ SRC/DBConnection.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 import MySQLdb as mdb
 from sshtunnel import SSHTunnelForwarder
 import simplejson as json
@@ -21,46 +20,46 @@ def execute_query(query, *kargs):
     :return:
     """
     rows = []
-    """
     # With SSH
     with SSHTunnelForwarder(('nova.cs.tau.ac.il', 22), ssh_password=MOODLE_PASSWORD, ssh_username=MOODLE_USERNAME,
                             remote_bind_address=('mysqlsrv1.cs.tau.ac.il', 3306)) as server:
         con = mdb.connect(host='127.0.0.1', port=server.local_bind_port, user='DbMysql03', passwd='DbMysql03')
-        ##### end With SSH
-    """
-    # Without SSH
-    con = mdb.connect(host='mysqlsrv1.cs.tau.ac.il', port=3306, user='DbMysql03', passwd='DbMysql03')
-    ##### end Without SSH
+    ##### end With SSH
 
-    con.select_db('DbMysql03')
-    cur = con.cursor()
-    with con:
+        # Without SSH
+        # con = mdb.connect(host='mysqlsrv1.cs.tau.ac.il', port=3306, user='DbMysql03', passwd='DbMysql03')
+        ##### end Without SSH
+
+
+        con.select_db('DbMysql03')
         cur = con.cursor()
-        try:
-            cur.execute(
-                query % kargs)  # query should be something like "SELECT %S FROM %S ..." and kargs are the params
-            con.commit()
-
-        except Exception as e:
-            print(e)
-            print("Error: failed executing/committing query")
-            con.rollback()
-            return
-        json_data = []
-        if 'INSERT' not in query:
+        with con:
+            cur = con.cursor()
             try:
-                row_headers = [x[0] for x in cur.description]
-                rows = cur.fetchall()
+                cur.execute(
+                    query % kargs)  # query should be something like "SELECT %S FROM %S ..." and kargs are the params
+                con.commit()
+
             except Exception as e:
                 print(e)
-                print("Error: failed fetching data")
+                print("Error: failed executing/committing query")
+                con.rollback()
+                return
+            json_data = []
+            if 'INSERT' not in query:
+                try:
+                    row_headers = [x[0] for x in cur.description]
+                    rows = cur.fetchall()
+                except Exception as e:
+                    print(e)
+                    print("Error: failed fetching data")
 
-            for result in rows:
-                json_data.append(dict(zip(row_headers, result)))
+                for result in rows:
+                    json_data.append(dict(zip(row_headers, result)))
 
-        cur.close()
+            cur.close()
 
 
-    return json.dumps(json_data)
+        return json.dumps(json_data)
 
     # return rows
