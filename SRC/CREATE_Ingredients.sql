0fe97c62c8074bebd62c7e306cbed7793d2a328e
yuvalzifruttau <33434382+yuvalzifruttau@users.noreply.github.com>
2019-01-22 15:07:16 +0200

    Delete CREATE_Ingredients.sql

--- SRC/CREATE_Ingredients.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `ingredients` (
-	`ingredient_name` VARCHAR(50) NOT NULL,
-	`description` VARCHAR(5000) NOT NULL,
-	`type` VARCHAR(50) NOT NULL,
-	`ingredient_img_url` VARCHAR(2083) NOT NULL,
-	`calories` INT(4) UNSIGNED NULL DEFAULT NULL,
-	PRIMARY KEY (`ingredient_name`)
-)
