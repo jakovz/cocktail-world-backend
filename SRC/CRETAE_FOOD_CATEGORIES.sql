1b7212d50931fea9d2fb0ba4ad66d8d459c5b8b9
yuvalzifruttau <33434382+yuvalzifruttau@users.noreply.github.com>
2019-01-22 15:07:33 +0200

    Delete CRETAE_FOOD_CATEGORIES.sql

--- SRC/CRETAE_FOOD_CATEGORIES.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `food_categories` (
-	`id` INT(3) NOT NULL,
-	`name` VARCHAR(100) NOT NULL,
-	`category_img_link` VARCHAR(2083) NULL DEFAULT NULL,
-	`description` VARCHAR(5083) NULL DEFAULT NULL,
-	PRIMARY KEY (`name`),
-	UNIQUE INDEX `id` (`id`)
-)
