9b4b0f8a30339b3e0087e27dfb4c32d75e3c6545
idoos <idoosams@gmail.com>
2019-01-22 13:56:32 +0200

    update creat

--- SRC/CREATE_COCKTAILS_INGREDIENTS.sql
+++ SRC/CREATE_COCKTAILS_INGREDIENTS.sql
@@ -2,5 +2,7 @@ CREATE TABLE `cocktails_ingredients` (
 	`cocktail_id` INT(5) NOT NULL,
 	`ingredient_name` VARCHAR(50) NOT NULL,
 	`measure` VARCHAR(50) NULL DEFAULT NULL,
-	PRIMARY KEY (`cocktail_id`, `ingredient_name`)
+	PRIMARY KEY (`cocktail_id`, `ingredient_name`),
+	INDEX `cocktailsIngredientsIndex` (`ingredient_name`),
+	CONSTRAINT `cocktail_id` FOREIGN KEY (`cocktail_id`) REFERENCES `drinks` (`id`)
 )
--- SRC/CRETAE_FOOD_CATEGORIES.sql
+++ SRC/CRETAE_FOOD_CATEGORIES.sql
@@ -1,10 +1,8 @@
 CREATE TABLE `food_categories` (
-	`id` INT NULL,
-	`category` VARCHAR(50) NULL,
-	`name` VARCHAR(100) NULL,
-	`category_img_link` VARCHAR(2083) NULL,
-	`description` VARCHAR(500) NULL
+	`id` INT(3) NOT NULL,
+	`name` VARCHAR(100) NOT NULL,
+	`category_img_link` VARCHAR(2083) NULL DEFAULT NULL,
+	`description` VARCHAR(5083) NULL DEFAULT NULL,
+	PRIMARY KEY (`name`),
+	UNIQUE INDEX `id` (`id`)
 )
-COMMENT='Food_categories\r\na.	id\r\nb.	category\r\nc.	name\r\nd.	category_img_link\r\ne.	description'
-COLLATE='utf8mb4_0900_ai_ci'
-;
